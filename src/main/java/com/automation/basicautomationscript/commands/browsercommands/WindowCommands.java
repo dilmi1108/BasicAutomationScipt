14663dc045e93a11e124a1b21dde6631159c1701
dilmi1108 <dilmihirunika2001@gmail.com>
2025-09-06 23:09:28 +0530

    Main.java added

---
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/WindowCommands.java
@@ -0,0 +1,43 @@
+package com.automation.basicautomationscript.commands.browsercommands;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+public class WindowCommands {
+    public static void main(String[] args) throws InterruptedException {
+        newTabbedWindow();
+    }
+
+    public static void newTabbedWindow() throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://demo.automationtesting.in/Windows.html");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Open New Tabbed Windows ']")).click();
+        Thread.sleep(3000);
+
+        //store the parent window
+        String parentWindow = driver.getWindowHandle();
+        System.out.println("Parent Window: " + parentWindow);
+
+        driver.findElement(By.xpath("//button[text()='    click   ']")).click();
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String window : allWindows) {
+            if (!window.equals(parentWindow)) {
+                driver.switchTo().window(window);
+                break;
+            }
+        }
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+        driver.switchTo().window(parentWindow);
+        System.out.println(driver.getTitle());
+
+    }
+}
