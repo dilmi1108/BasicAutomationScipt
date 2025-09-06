6bfe14d535db045a4ae87128d8f2e74c6a44246c
dilmi1108 <dilmihirunika2001@gmail.com>
2025-09-07 01:51:15 +0530

    WaitCommands.java updated

--- src/main/java/com/automation/basicautomationscript/commands/browsercommands/WaitCommands.java
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/WaitCommands.java
@@ -3,12 +3,17 @@ package com.automation.basicautomationscript.commands.browsercommands;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class WaitCommands {
     public static void main (String [] args){
-        implicitWait();
+       // implicitWait();
+        explicitWait();
     }
 
     public static void implicitWait(){
@@ -18,5 +23,16 @@ public class WaitCommands {
         driver.get("https://demoqa.com/elements");
         driver.findElement(By.xpath("//span[text() ='Text Box']")).click();
         System.out.println("click on the text box");
+    }
+    public static void explicitWait(){
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://demoqa.com/dynamic-properties");
+        WebElement enableAfter = wait.until(ExpectedConditions.elementToBeClickable(By.id("enableAfter")));
+        enableAfter.click();
+        System.out.println("click on  enableAfter");
+
     }
 }
