5e73577d734c593adc44f91a800707c544c6edd3
dilmi1108 <dilmihirunika2001@gmail.com>
2025-09-07 10:08:14 +0530

    WaitCommands.java updated

--- src/main/java/com/automation/basicautomationscript/commands/browsercommands/WaitCommands.java
+++ src/main/java/com/automation/basicautomationscript/commands/browsercommands/WaitCommands.java
@@ -2,18 +2,21 @@ package com.automation.basicautomationscript.commands.browsercommands;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class WaitCommands {
     public static void main (String [] args){
-       // implicitWait();
-        explicitWait();
+       //implicitWait();
+       //explicitWait();
+       fluentWait();
     }
 
     public static void implicitWait(){
@@ -33,6 +36,19 @@ public class WaitCommands {
         WebElement enableAfter = wait.until(ExpectedConditions.elementToBeClickable(By.id("enableAfter")));
         enableAfter.click();
         System.out.println("click on  enableAfter");
+    }
+    public static void fluentWait(){
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
 
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                        .pollingEvery(Duration.ofSeconds(2))
+                                .ignoring(NoSuchElementException.class);
+
+        driver.get("https://demoqa.com/dynamic-properties");
+        WebElement visibleAfter = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("visibleAfter")));
+        visibleAfter.click();
+        System.out.println(visibleAfter.getText());
     }
 }
