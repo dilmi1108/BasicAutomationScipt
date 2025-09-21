3ee4f2ce4cded3749f47de595c6d04e90dd7ad1f
dilmi1108 <dilmihirunika2001@gmail.com>
2025-09-21 08:10:53 +0530

    extent report added to pom.xml , ExtentReportManager.java class added

---
+++ src/test/java/reporter/ExtentReportManager.java
@@ -0,0 +1,47 @@
+package reporter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import java.util.Objects;
+
+public class ExtentReportManager {
+    private static ExtentReports extent;
+    private static ExtentTest test;
+
+    public static void initReport(){
+        extent = new ExtentReports();
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("./reports/ExtentSparkReport.html");
+        sparkReporter.config().setTheme(Theme.STANDARD);
+        sparkReporter.config().setDocumentTitle("Ebay Report");
+        sparkReporter.config().setReportName("Ebay Report Result");
+        extent.attachReporter(sparkReporter);
+    }
+    public static void createTest(String testName){
+        test = extent.createTest(testName);
+    }
+
+    public static void logPass(String message) {
+        if (Objects.nonNull(test)) {
+            test.log(Status.PASS, message);
+        }
+    }
+        public static void logFail(String message){
+        if(Objects.nonNull(test)) {
+            test.log(Status.FAIL, message);
+        }
+    }
+        public static void logSkip(String message) {
+            if (Objects.nonNull(test)) {
+                test.log(Status.SKIP, message);
+            }
+        }
+        public static void logInfo(String message) {
+            if (Objects.nonNull(test)) {
+                test.log(Status.INFO, message);
+            }
+        }
+}
